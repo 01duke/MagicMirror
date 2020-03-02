505e63f0d57aa4f240a0563e6647e2530fbd0717
O1duke <idahodukes@hotmail.com>
2020-03-02 15:49:51 -0700

    mmm google photos

--- config/config.js
+++ config/config.js
@@ -9,21 +9,13 @@
  */
 
 var config = {
-<<<<<<< HEAD
 	address: "0.0.0.0", // Address to listen on, can be:
-=======
-	address: "localhost", // Address to listen on, can be:
->>>>>>> e9e2dc55b64406a6eba38dfcb08286a34f39a5f9
 	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 	// - another specific IPv4/6 to listen on a specific interface
 	// - "", "0.0.0.0", "::" to listen on any interface
 	// Default, when address config is left out, is "localhost"
 	port: 8080,
-<<<<<<< HEAD
 	ipWhitelist: [], // Set [] to allow all IP addresses
-=======
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
->>>>>>> e9e2dc55b64406a6eba38dfcb08286a34f39a5f9
 	// or add a specific IPv4 of 192.168.1.5 :
 	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -51,10 +43,12 @@ var config = {
 		},
 		{
 			module: "calendar",
-			header: "US Holidays",
+			header: "UP COMING EVENTS",
 			position: "top_left",
 			config: {
 				calendars: [{
+					maximumEntries:"15",
+					fade: "false",
 					symbol: "calendar-check",
 					url: "https://calendar.google.com/calendar/ical/shane.duke88%40gmail.com/private-6099dddf6f4ad23946a53b1d9f57d728/basic.ics"
 				}]
@@ -66,10 +60,10 @@ var config = {
 		// npm install
 		{
 			module: "MMM-GooglePhotos",
-			position: "middle_center",
+			position: "bottom_center",
 			config: {
 
-				albumId: ["ALBUM_ID1", "ALBUM_ID2"], // your album id(s) from result of `auth_and_test.js`
+				albumId: ["AIniX88YjZ__20KsVuZMc0TCQ42ffFCwDJJR8CBh-Oqr9muGC_ackOlTtxCmBibQqZJHOQWgO4DU"], // your album id(s) from result of `auth_and_test.js`
 				refreshInterval: 1000 * 60, // Number of milliseconds before showing a different photo
 				scanInterval: 1000 * 60 * 10, // too many scans might cause API quota limit also
 				//note(2018-07-29). It is some weird. API documents said temporal image url would live for 1 hour, but it might be broken shorter. So, per 10 min scanning could prevent dead url.
@@ -100,32 +94,13 @@ var config = {
 			position: "top_right",
 			header: "Weather Forecast",
 			config: {
+				fade: "false",
 				location: "Idaho Falls",
 				locationID: "5596475", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
 				appid: "9ad9ed31fe50ebed3f38ca3907a8dba7"
 			}
-		},
-		{
-			module: "MMM-GooglePhotos",
-			position: "bottom_right",
-			config: {
+		}
 
-				albumId: ["ALBUM_ID1", "ALBUM_ID2"], // your album id(s) from result of `auth_and_test.js`
-				refreshInterval: 1000 * 60, // Number of milliseconds before showing a different photo
-				scanInterval: 1000 * 60 * 10, // too many scans might cause API quota limit also
-				//note(2018-07-29). It is some weird. API documents said temporal image url would live for 1 hour, but it might be broken shorter. So, per 10 min scanning could prevent dead url.
-
-				sort: "time", //'time', 'reverse', 'random'
-				showWidth: "800px", // how large the photo will be shown as. (e.g;'100%' for fullscreen)
-				showHeight: "600px",
-				originalWidthPx: 800, // original size of loaded image. (related with image quality)
-				originalHeightPx: 600, // Bigger size gives you better quality, but can give you network burden
-				opacity: 1, // target "opacity" property (https://www.w3schools.com/cssref/css3_pr_opacity.asp)
-				mode: "hybrid", // "cover" or "contain" (https://www.w3schools.com/cssref/css3_pr_background-size.asp)
-				// "hybrid": will change "cover" and "contain" automatically based on aspect ratio
-				showDateLabel: true, // If True, shows a label of how long ago the photo was taken (e.g. 2 years ago, 7 days ago, etc...)
-			}
-		},
 	]
 
 };
